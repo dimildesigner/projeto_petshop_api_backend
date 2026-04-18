26e690ea2c255ddb57e7b6f0a01d6f6b383d1222
dimildesigner <dimildesignerdev@gmail.com>
2026-04-18 13:10:01 -0300

    fix: enable CORS

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
+
 const app = express();
 
-app.get("/auth", (req, res) => {
-  res.send("AUTH OK");
-});
+app.use(cors());
 app.use(express.json());
+
+// Rota de teste abaixo:
+// app.get("/auth", (req, res) => {
+//   res.send("AUTH OK");
+// });
+
 app.use("/products", productRoutes);
 app.use("/auth", authRoutes);
 
