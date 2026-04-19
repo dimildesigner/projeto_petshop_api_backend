0d452653782fdbe4e1eb37bd42b55d1832810ee2
dimildesigner <dimildesignerdev@gmail.com>
2026-04-19 02:32:00 -0300

    ajustes e configurações

--- cloudinary.js
+++ cloudinary.js
@@ -1,1 +1,5 @@
-console.log("Cloudinary:", process.env.dswxda39i);
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
