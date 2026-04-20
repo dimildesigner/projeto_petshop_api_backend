b0675f9c5e4d75f69e4cc746ccdf9974b54a16e3
dimildesigner <dimildesignerdev@gmail.com>
2026-04-19 23:35:00 -0300

    fix: adiciona webp nos formatos permitidos no upload

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -6,7 +6,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "petshop",
-    allowed_formats: ["jpg", "png", "jpeg"],
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
