e2d2222d0cecc65207889f0e1b4215dd74e4351b
dimildesigner <dimildesignerdev@gmail.com>
2026-04-21 18:14:16 -0300

    fix: remove next do pre save do User para compatibilidade com Mongoose 7+

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
+  nome:  { type: String, required: true },
   email: { type: String, required: true, unique: true },
   senha: { type: String, required: true },
   role: {
@@ -15,10 +15,9 @@ const userSchema = new mongoose.Schema({
   timestamps: true,
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("senha")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("senha")) return;
   this.senha = await bcrypt.hash(this.senha, 10);
-  next();
 });
 
 userSchema.methods.verificarSenha = async function (senha) {
